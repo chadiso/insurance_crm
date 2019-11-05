22d85368fcf8aac92f704697fba01433ca8fc132
Roman Osidach <hard.prophecy@gmail.com>
2019-11-05 23:29:16 +0200

    Secure staging environment

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   require "sidekiq/web"
+
+  # Sidekiq Basic Auth from routes on production environment
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_AUTH_USERNAME"])) &
+        ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_AUTH_PASSWORD"]))
+  end if Rails.env.production?
+
   mount Sidekiq::Web, at: "/sidekiq"
 
   resources :imports, only: %i[new create show index destroy]
